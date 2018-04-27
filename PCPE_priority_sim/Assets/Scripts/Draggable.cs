0246857708e133ab483925b1fec7adb980703058
Robert <packleader@dumbdogentertainment.com>
2018-04-27 07:27:21 -0500

    -- tutorial progress;

---
+++ PCPE_priority_sim/Assets/Scripts/Draggable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    public Transform parentToReturnTo = null;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        this.parentToReturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+
+        this.GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End drag ..");
+        this.transform.parent = this.parentToReturnTo;
+
+        this.GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+}
---
+++ PCPE_priority_sim/Assets/Scripts/DropZone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + " was dropped on to " + this.gameObject.name);
+
+        eventData.pointerDrag.GetComponent<Draggable>().parentToReturnTo = this.transform;
+    }
+}
